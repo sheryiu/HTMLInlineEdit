c8fe30ec462dc7c470db6fa3fbab5713c2f2cf56
yiu.cheuk.lok <yiu.cheuk.lok@gmail.com>
2017-06-24 12:27:41 -0400

    More functions

--- inline-edit.js
+++ inline-edit.js
@@ -11,13 +11,14 @@ function inlineDefaultUpdateCell(cell, i, rowName, finishFunc) {
 	if (cell.dataset.inlinemode == "button") {
 		cellContent += "<input type='submit' value='Finish' form='"+rowName+"Form'/>";
 	} else {
+		cellContent += "<input type='text' value='"+cell.dataset.inlinevalue+"' form='"+rowName+"Form'/>";
 		if (i === 0) {
 			cellContent += "<form id='"+rowName+"Form'></form>";
 		}
-		cellContent += "<input type='text' value='"+cell.dataset.inlinevalue+"' form='"+rowName+"Form'/>";
 	}
 	cell.innerHTML = cellContent;
 	if (i === 0) {
+		// set the onsubmit function of the form of this row
 		document.getElementById(rowName+"Form").onsubmit = function () {
 			event.preventDefault();
 			if (this.reportValidity()) {
@@ -34,20 +35,23 @@ function inlineDefaultFinish(rowName) {
 		var cell = tableRow.children[i];
 		if (cell.dataset.inlinemode != "button") {
 			rowData[cell.dataset.inlinename] = cell.children[0].value;
+			cell.dataset.inlinevalue = cell.children[0].value;
 		}
 	}
 	console.log(rowData);
+	
+	for (i=0; i<tableRow.childElementCount; i++) {
+		var cell = tableRow.children[i];
+		inlineDefaultFinishCell(cell, i, rowName);
+	}
 }
 
 function inlineDefaultFinishCell(cell, i, rowName) {
 	var cellContent = "";
 	if (cell.dataset.inlinemode == "button") {
-		cellContent += "<input type='submit' value='Finish' form='"+rowName+"Form'/>";
+		cellContent += "<input type='button' value='Edit'/>";
 	} else {
-		if (i === 0) {
-			cellContent += "<form id='"+rowName+"Form' onSubmit=''></form>";
-		}
-		cellContent += "<input type='text' value='"+cell.dataset.inline+"' form='"+rowName+"Form'/>";
+		cellContent += cell.dataset.inlinevalue;
 	}
 	cell.innerHTML = cellContent;
 }
