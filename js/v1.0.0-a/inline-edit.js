4054d3fb63bbeebe2d36b3570704aa0527288bae
yiu.cheuk.lok <yiu.cheuk.lok@gmail.com>
2017-06-26 10:56:03 -0400

    Add files

---
+++ js/v1.0.0-a/inline-edit.js
@@ -0,0 +1,72 @@
+var inlineEditButtonContent = {};
+
+function inlineEdit(rowName, options) {
+	var tableRow = document.getElementById(rowName);
+	for (var i=0; i<tableRow.childElementCount; i++) {
+		var cell = tableRow.children[i];
+		if (options.hasOwnProperty("updateCell"))
+			options.updateCell(cell, i, rowName, options);
+		else
+			inlineDefaultUpdateCell(cell, i, rowName, options);
+	}
+}
+
+function inlineDefaultUpdateCell(cell, i, rowName, options) {
+	var cellContent = "";
+	if (cell.dataset.inlinemode == "button") {
+		inlineEditButtonContent[rowName] = cell.innerHTML;
+		cellContent += "<input type='submit' value='Finish' form='"+rowName+"Form'/>";
+	} else {
+		cellContent += "<input type='text' value='"+cell.dataset.inlinevalue+"' form='"+rowName+"Form'/>";
+		if (i === 0) {
+			cellContent += "<form id='"+rowName+"Form'></form>";
+		}
+	}
+	cell.innerHTML = cellContent;
+	if (i === 0) {
+		// set the onsubmit function of the form of this row
+		document.getElementById(rowName+"Form").onsubmit = function () {
+			event.preventDefault();
+			if (this.reportValidity()) {
+				if (options.hasOwnProperty("finish"))
+					options.finish(rowName, options);
+				else
+					inlineDefaultFinish(rowName, options);
+			}
+		};
+	}
+}
+
+function inlineDefaultFinish(rowName, options) {
+	var tableRow = document.getElementById(rowName);
+	var rowData = {};
+	for (var i=0; i<tableRow.childElementCount; i++) {
+		var cell = tableRow.children[i];
+		if (cell.dataset.inlinemode != "button") {
+			rowData[cell.dataset.inlinename] = cell.children[0].value;
+			cell.dataset.inlinevalue = cell.children[0].value;
+		}
+	}
+	
+	// do whatever ajax magic
+	if (options.hasOwnProperty("finishCallback"))
+		options.finishCallback(rowData, rowName);
+	
+	for (i=0; i<tableRow.childElementCount; i++) {
+		var cell = tableRow.children[i];
+		if (options.hasOwnProperty("finishCell"))
+			options.finishCell(cell, i, rowName);
+		else
+			inlineDefaultFinishCell(cell, i, rowName);
+	}
+}
+
+function inlineDefaultFinishCell(cell, i, rowName) {
+	var cellContent = "";
+	if (cell.dataset.inlinemode == "button") {
+		cellContent += inlineEditButtonContent[rowName];
+	} else {
+		cellContent += cell.dataset.inlinevalue;
+	}
+	cell.innerHTML = cellContent;
+}
